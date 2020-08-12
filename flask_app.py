07b72329d42d587025e453d00050845f43945932
Korobov, Alexey <akorobov@kpmg.ru>
2020-08-12 22:31:14 +0300

    add methods

--- flask_app.py
+++ flask_app.py
@@ -1,11 +1,26 @@
 
 # A very simple Flask Hello World app for you to get started with...
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+app.config["DEBUG"] = True
 
 @app.route('/')
 def hello_world():
-    return 'project1'
+    return 'project2'
 
+@app.route('/list')
+def list():
+    return render_template('list.html')
+
+@app.route('/init')
+def initTable():
+    try:
+        return 'init ok'
+    except expression as ex:
+        return ex
+
+@app.route("/insert", methods=["POST"])
+def insert():
+    return 'ok'

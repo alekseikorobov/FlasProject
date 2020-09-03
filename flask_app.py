f6e8e82301700ba2d990c6614ad4033c09020e83
Korobov, Alexey <akorobov@kpmg.ru>
2020-09-03 11:32:32 +0300

    add method for upload and create db

--- flask_app.py
+++ flask_app.py
@@ -2,6 +2,7 @@
 # A very simple Flask Hello World app for you to get started with...
 
 from flask import Flask, render_template,send_from_directory
+from flask_sqlalchemy import SQLAlchemy
 
 import json
 import os
@@ -11,6 +12,30 @@ cfg = json.load(open("/home/alekseikorobov/mysite/config.json", "r"))
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
+
+
+SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
+    username="alekseikorobov",
+    password="vJ4XMPQm",
+    hostname="alekseikorobov.mysql.pythonanywhere-services.com",
+    databasename="catalog",
+)
+app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
+app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
+
+
+class Stats(db.Model):
+
+    __tablename__ = "Stats"
+
+    id = db.Column(db.Integer, primary_key=True)
+    message = db.Column(db.String(4096))
+    date = db.Column(db.DateTime)
+    machineName = db.Column(db.String(4096))
+
 
 @app.route('/')
 def index():
@@ -45,5 +70,20 @@ def upload(filename):
         response = send_from_directory(directory='.',filename=filename,as_attachment=True)
         response.cache_control.max_age = 5  # e.g. 1 minute
         return response
+    except Exception as ex:
+        return str(ex)
+
+@app.route('/UploadStat',methods=["POST"])
+def UploadStat():
+    try:
+        data = request.args.get('data')
+        machineName = request.args.get('machineName')
+
+
+        # stat = Stats(machineName=machineName,)
+        # db.session.add(comment)
+        # db.session.commit()
+
+        return data+'|||'+machineName
     except Exception as ex:
         return str(ex)
